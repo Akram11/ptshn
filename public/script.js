e6ebc51355514478738652e59475ee2377beeec7
akram11 <akram.f11@gmail.com>
2020-09-04 17:45:46 +0200

    added a canvas, where a user can sign; clear button

--- public/script.js
+++ public/script.js
@@ -1,27 +1,41 @@
-// console.log("this is the linked scripts");
-// let body = document.getElementsByTagName("body");
-// body.style.backgroundColor = "red";
+let drawing = false;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-// $("form").css("background-color", "yellow");
-$("document").on("load", () => {
-    console.log("inside load");
-    var canvas = document.getElementById("tutorial");
-    if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "rgb(200, 0, 0)";
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(30, 30, 50, 50);
-    }
+canvas.addEventListener("mousedown", function (e) {
+    beginDrawing(e);
 });
 
-$(window).load(function () {
-    console.log("inside load");
-    var canvas = $("canvas");
-    console.log(canvas);
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    canvas.css("border", "1px solid black");
+canvas.addEventListener("mousemove", function (e) {
+    doDrawing(e);
+});
+canvas.addEventListener("mouseup", endDrawing);
+canvas.addEventListener("mouseleave", endDrawing);
+
+function beginDrawing(e) {
+    drawing = true;
+    console.log("begin drawing");
+    ctx.beginPath();
+    ctx.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+}
+
+function doDrawing(e) {
+    if (drawing) {
+        ctx.strokeStyle = "black";
+        ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+        ctx.stroke();
+    }
+}
+
+function endDrawing() {
+    drawing = false;
+}
+
+function clearCanvas() {
+    console.log("cear");
+}
+
+$("#clear").click((e) => {
+    e.preventDefault();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
